821a56d0be81f3c4418ca76f8beaf999f1ccc2d2
wuyunlai <wuyunlai@163.com>
2012-03-05 15:08:53 +0800

    add HelloGit

---
+++ src/com/wuyl/test/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wuyl.test;
+
+/**
+ * @author wuyl
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
